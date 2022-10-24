47a62c9b6277edbb93cb3c7872cfd532f907ec58
Danielateresa <dany.t_87@hotmail.it>
2022-10-24 18:51:49 +0200

    Final commit snack 5

--- snack_5/assets/js/main.js
+++ snack_5/assets/js/main.js
@@ -8,7 +8,7 @@ compresa tra “a” e “b” */
 //const numA < numB
 //const numB <= list.length;
 const numA = 2;
-const list = [3, 5, 6, 8, 9, 1];
+const list = [3, 2, 6, 8, 9, 1];
 const numB = 4;
 
 
@@ -18,8 +18,8 @@ function newArray(array, num1, num2) {
 
     let generatedList = [];
 
-    array.forEach((element, index) => {
-        if (element !== num1.lenght + 1 && element >= num2) {
+    array.forEach((element, index, array) => {
+        if (array[index] > num1 && array[index] >= num2 + 1) {
             generatedList.push(array[index])
         }
     });
