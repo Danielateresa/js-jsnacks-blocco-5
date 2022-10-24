3f82a5e2fff95d73e5eafb65118d00d131b82ea3
Danielateresa <dany.t_87@hotmail.it>
2022-10-24 13:36:10 +0200

    Initial commit snack blocco 5 | snack 1

---
+++ snack_1/assets/js/main.js
@@ -0,0 +1,56 @@
+/* Crea un array di 10 oggetti che rappresentano una zucchina, indicando
+per ognuna varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine. */
+
+const zucchine = [
+    {
+        varieta: 'tonda',
+        peso: 1,
+        lunghezza: 10
+    },
+    {
+        varieta: 'napoletana',
+        peso: 3,
+        lunghezza: 10
+    },
+    {
+        varieta: 'tonda',
+        peso: 2,
+        lunghezza: 10
+    },
+    {
+        varieta: 'trombetta',
+        peso: 1,
+        lunghezza: 10
+    },
+    {
+        varieta: 'nera',
+        peso: 1,
+        lunghezza: 10
+    },
+    {
+        varieta: 'tonda',
+        peso: 3,
+        lunghezza: 10
+    },
+    {
+        varieta: 'napoletana',
+        peso: 1,
+        lunghezza: 10
+    },
+    {
+        varieta: 'tonda',
+        peso: 4,
+        lunghezza: 10
+    },
+    {
+        varieta: 'trombetta',
+        peso: 1,
+        lunghezza: 10
+    },
+    {
+        varieta: 'nera',
+        peso: 2,
+        lunghezza: 10
+    }
+]
